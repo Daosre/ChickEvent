d8682ea6cfce0947e854b8a0a69649d08dbd737f
Daorse <mmorpgplays5@gmail.com>
2024-04-16 16:46:01 +0200

    Fin de journée

--- Front/Js Front/AllPost.js
+++ Front/Js Front/AllPost.js
@@ -1,3 +1,9 @@
+let AddEvent = document.querySelector('.AddEvent')
+
+AddEvent.addEventListener('click', () => {
+    window.location.href = '../AddPost/AddPost.html'
+})
+
 async function getAllEvent() {
     let response = await fetch('http://127.0.0.1:3107/Event/AllEvent')
     let client = await response.json()
@@ -5,11 +11,11 @@ async function getAllEvent() {
 
     for(const Event of client) {
         let NewEvent = document.createElement('div')
+        NewEvent.classList.add('Event')
         NewEvent.innerHTML = `<h2 class='Title'>${Event.title}</h2>
         <p class='Description'>${Event.description}</p>
         <img src='${Event.image} class='EventImage'/>
         <p class='category'>${Event.category}</p>`
-
         emplacement.appendChild(NewEvent)
     }
 }
