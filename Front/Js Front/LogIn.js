d77f60de1c8344c6981652f73d14b84d3786ffcb
Daorse <mmorpgplays5@gmail.com>
2024-04-16 16:07:30 +0200

    LogIn vérifié

---
+++ Front/Js Front/LogIn.js
@@ -0,0 +1,34 @@
+let submit = document.querySelector('.submit')
+async function handleLogin() {
+    let email = document.querySelector(".email").value;
+    let password = document.querySelector(".Vérif1").value;
+    let error = document.querySelector(".error");
+  
+    let LogIn = {
+      email: email,
+      password: password,
+    };
+    let request = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(LogIn),
+    };
+    //LogIn
+    let apiRequest = await fetch("http://127.0.0.1:3107/user/LogIn", request)
+    let response = await apiRequest
+    if (response.status === 200) {
+      user = await response.json()
+    } else {
+      error.innerHTML = "Something is Wrong"
+    }
+  }
+
+  submit.addEventListener('click', () => {
+    handleLogin()
+    submit.innerHTML += `<span class="SuccesFull">Connexion Réussi<span>`
+    setTimeout(() => {
+        window.location.href = '../AllPost/AllEvent.html'
+    }, 2000);
+})
