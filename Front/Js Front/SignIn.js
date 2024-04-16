f0a0c6aa1bbd4fdef8caa580c3c880561e7a579a
Daorse <mmorpgplays5@gmail.com>
2024-04-16 15:18:04 +0200

    SignIn Valider

--- Front/Js Front/SignIn.js
+++ Front/Js Front/SignIn.js
@@ -1,16 +1,16 @@
 let id
 let role
-
+let submit = document.querySelector('.submit')
 async function handLeRegister() {
-    let first_name = document.querySelector('.first_name').value
-    let last_name = document.querySelector('.last_name').value
+    let firstName = document.querySelector('.first_name').value
+    let lastName = document.querySelector('.last_name').value
     let email = document.querySelector('.email').value
     let password = document.querySelector('.Vérif1').value
     let error = document.querySelector('.error')
 
-    let newUser = {
-        first_name: first_name,
-        last_name: last_name,
+    let User = {
+        firstName: firstName,
+        lastName: lastName,
         email: email,
         password: password
     }
@@ -20,18 +20,24 @@ async function handLeRegister() {
         headers: {
           "Content-type": "application/json; charset=utf-8",
         },
-        body: JSON.stringify(newUser),
+        body: JSON.stringify(User),
       };
 
-      let apiRequest = await fetch("http://127.0.0.1:3107/SignUp")
+      let apiRequest = await fetch("http://127.0.0.1:3107/user/SignUp", request)
       let response = await apiRequest
       if (response.status === 200) {
         user = await response.json()
-        localStorage.setItem('session', user.id)
-        localStorage.setItem('session', user.role)
+        localStorage.setItem('session', User.id)
+        localStorage.setItem('session', User.role)
       } else {
         error.innerHTML = "Something Miss"
       }
-
-
 }
+//Au clique toute la vérification pour enregistrer la personne ce fait fait apparaitre un message comme quoi inscription est réussi puis envoie sur la page de connexion
+submit.addEventListener('click', () => {
+    handLeRegister()
+    submit.innerHTML += `<span class="SuccesFull">Inscription Réussi<span>`
+    setTimeout(() => {
+        window.location.href = '../LogIn/LogIn.html'
+    }, 2000);
+})
